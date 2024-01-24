a955e6609d08d30f57a4cc53a2627924c85f5d04
Philip Roche <phil.roche@canonical.com>
2024-01-24 15:59:50 +0000

    Do not iterate on pocket and publication status
    A (package, version, arch) tuple is unique within an archive, not just an archive and series.
    
    This makes querying much easier and quicker.
    
    Also migrated to the filenames defined by the API for the changes and the buildinfo files

--- ubuntu_package_buildinfo/cli.py
+++ ubuntu_package_buildinfo/cli.py
@@ -3,50 +3,32 @@
 import faulthandler
 import hashlib
 import logging
-import re
 import sys
 
 import click
 
-import urllib.parse
 from launchpadlib.launchpad import Launchpad
 from launchpadlib.uris import service_roots
 
-# Which archive pockets are checked in order of where a recent package is most likely to be published
-ARCHIVE_POCKETS = ["Updates", "Security", "Release", "Proposed"]
 faulthandler.enable()
 
 
-def _get_binary_packages(archive, version, binary_package_name, lp_arch_series, pocket, status="Published"):
+def _get_binary_packages(archive, version, binary_package_name, lp_arch_series):
     binaries = archive.getPublishedBinaries(
         exact_match=True,
         version=version,
         binary_name=binary_package_name,
         distro_arch_series=lp_arch_series,
-        pocket=pocket,
-        order_by_date=True,
-        status=status,
+        order_by_date=True
     )
     return binaries
-
-
-def _get_published_sources(archive, version, source_package_name, lp_series, pocket, status="Published"):
-    sources = archive.getPublishedSources(
-        exact_match=True,
-        version=version,
-        source_name=source_package_name,
-        pocket=pocket,
-        distro_series=lp_series,
-        order_by_date=True,
-        status=status,
-    )
-    return sources
 
 
 def _write_build_artifact_to_file(filename, content, message):
     with open(filename, 'w') as f:
         f.write(content)
         print(message)
+
 
 def get_buildinfo(
     package_series, package_name, package_version, package_architecture="amd64", ppas=[], lp_user=None
@@ -77,87 +59,73 @@ def get_buildinfo(
 
     ubuntu = launchpad.distributions["ubuntu"]
     build_found = False
-    for pocket in ARCHIVE_POCKETS:
+    # TODO add support for PPAs
+    # if args.ppa:
+    #     ppa_owner, ppa_name = args.ppa.split('/')
+    #     archive = launchpad.people[ppa_owner].getPPAByName(name=ppa_name)
+    #     if args.pocket != 'Release':
+    #         print('using pocket "Release" when using a PPA ...')
+    #         pocket = 'Release'
+    # else:
+    archive = ubuntu.main_archive
 
-        # TODO add support for PPAs
-        # if args.ppa:
-        #     ppa_owner, ppa_name = args.ppa.split('/')
-        #     archive = launchpad.people[ppa_owner].getPPAByName(name=ppa_name)
-        #     if args.pocket != 'Release':
-        #         print('using pocket "Release" when using a PPA ...')
-        #         pocket = 'Release'
-        # else:
-        archive = ubuntu.main_archive
+    lp_series = ubuntu.getSeries(name_or_version=package_series)
+    lp_arch_series = lp_series.getDistroArchSeries(archtag=package_architecture)
 
-        lp_series = ubuntu.getSeries(name_or_version=package_series)
-        lp_arch_series = lp_series.getDistroArchSeries(archtag=package_architecture)
+    # attempt to find a binary package build of this name first
+    binaries = _get_binary_packages(
+        archive, package_version, package_name, lp_arch_series
+    )
 
-        for package_publication_status in ["Published", "Superseded"]:
-            # attempt to find a binary package build of this name first
-            binaries = _get_binary_packages(
-                archive, package_version, package_name, lp_arch_series, pocket, status=package_publication_status
-            )
-
-            if len(binaries):
-                print(
-                    f"INFO: \tFound binary package "
-                    f"{package_name} {package_architecture} version {package_version} with {package_publication_status} status in {package_series} {pocket}."
-                )
-                binary_build_link = binaries[0].build_link
-                binary_build = launchpad.load(binary_build_link)
-                build_found = True
-                changesfile_url = binary_build.changesfile_url
-                buildinfo_url = binary_build.buildinfo_url
-                buildinfo_remote_filename = buildinfo_url.split("/")[-1]
-                buildlog_url = binary_build.build_log_url
-                changesfile_resp = launchpad._browser.get(changesfile_url).decode("utf-8", errors="ignore")
-                buildinfo_resp = launchpad._browser.get(buildinfo_url).decode("utf-8", errors="ignore")
-                buildlog_resp = launchpad._browser.get(buildlog_url).decode("utf-8", errors="ignore")
-
-
-                changes_filename = f"{package_name}_{package_version}_{package_architecture}_{package_series}.changes"
-                changes_msg = f"INFO: \tchanges written to {changes_filename}"
-                _write_build_artifact_to_file(changes_filename, changesfile_resp, changes_msg)
-
-                buildinfo_filename = f"{package_name}_{package_version}_{package_architecture}_{package_series}.buildinfo"
-                buildinfo_msg = f"INFO: \tbuildinfo written to {buildinfo_filename}"
-                _write_build_artifact_to_file(buildinfo_filename, buildinfo_resp, buildinfo_msg)
-
-                buildlog_filename = f"{package_name}_{package_version}_{package_architecture}_{package_series}.buildlog"
-                buildlog_msg = f"INFO: \tbuildlog written to {buildlog_filename}"
-                _write_build_artifact_to_file(buildlog_filename, buildlog_resp, buildlog_msg)
-
-                # find the hashes of buildinfo_filename in the changesfile_resp and verify that they match hash
-                # of the buildinfo_filename file already written to disk
-                sha256_checksums_found = False
-                for changesfile_line in changesfile_resp.splitlines():
-                    if "Checksums-Sha256:" in changesfile_line:
-                        sha256_checksums_found = True
-                    if sha256_checksums_found and buildinfo_remote_filename in changesfile_line:
-                        # get the hash from the changesfile_line
-                        changesfile_buildinfo_hash = changesfile_line.split()[0]
-                        # get the hash of the buildinfo content and compare it to the hash in the changes file
-                        sha256hash = hashlib.sha256(buildinfo_resp.encode("UTF-8")).hexdigest()
-                        if changesfile_buildinfo_hash == sha256hash:
-                            print(f"INFO: \tHash of {buildinfo_remote_filename} matches hash in changes file.")
-                        else:
-                            print(f"**********ERROR: \tHash of {buildinfo_remote_filename} does not match hash in changes file.")
-                        # we have found the hash of the buildinfo_filename in the changes file so we can stop
-                        # iterating over the changesfile lines
-                        break
-            else:
-                print(
-                    f"INFO: \tNo {package_publication_status} binaries found for {package_name}  {package_architecture} version {package_version} in {package_series} {pocket}"
-                )
-            if build_found:
-                # if we have found the binary package we can stop iterating over the publication statuses
-                break
-        if build_found:
-            # if we have found the source package we can stop iterating over the pockets
-            break
-    if not build_found:
+    if len(binaries):
         print(
-            f"**********ERROR: \tNo Published or Superseded binaries found for {package_name} {package_architecture} version {package_version} in {package_series} any {ARCHIVE_POCKETS} pocket."
+            f"INFO: \tFound binary package "
+            f"{package_name} {package_architecture} version {package_version} with in {package_series}."
+        )
+        binary_build_link = binaries[0].build_link
+        binary_build = launchpad.load(binary_build_link)
+        changesfile_url = binary_build.changesfile_url
+        buildinfo_url = binary_build.buildinfo_url
+
+        buildlog_url = binary_build.build_log_url
+        changesfile_resp = launchpad._browser.get(changesfile_url).decode("utf-8", errors="ignore")
+        buildinfo_resp = launchpad._browser.get(buildinfo_url).decode("utf-8", errors="ignore")
+        buildlog_resp = launchpad._browser.get(buildlog_url).decode("utf-8", errors="ignore")
+
+
+        changes_filename = changesfile_url.split("/")[-1]
+        changes_msg = f"INFO: \tchanges written to {changes_filename}"
+        _write_build_artifact_to_file(changes_filename, changesfile_resp, changes_msg)
+
+        buildinfo_filename = buildinfo_url.split("/")[-1]
+        buildinfo_msg = f"INFO: \tbuildinfo written to {buildinfo_filename}"
+        _write_build_artifact_to_file(buildinfo_filename, buildinfo_resp, buildinfo_msg)
+
+        buildlog_filename = f"{package_name}_{package_version}_{package_architecture}.buildlog"
+        buildlog_msg = f"INFO: \tbuildlog written to {buildlog_filename}"
+        _write_build_artifact_to_file(buildlog_filename, buildlog_resp, buildlog_msg)
+
+        # find the hashes of buildinfo_filename in the changesfile_resp and verify that they match hash
+        # of the buildinfo_filename file already written to disk
+        sha256_checksums_found = False
+        for changesfile_line in changesfile_resp.splitlines():
+            if "Checksums-Sha256:" in changesfile_line:
+                sha256_checksums_found = True
+            if sha256_checksums_found and buildinfo_filename in changesfile_line:
+                # get the hash from the changesfile_line
+                changesfile_buildinfo_hash = changesfile_line.split()[0]
+                # get the hash of the buildinfo content and compare it to the hash in the changes file
+                sha256hash = hashlib.sha256(buildinfo_resp.encode("UTF-8")).hexdigest()
+                if changesfile_buildinfo_hash == sha256hash:
+                    print(f"INFO: \tHash of {buildinfo_filename} matches hash in changes file.")
+                else:
+                    print(f"**********ERROR: \tHash of {buildinfo_filename} does not match hash in changes file.")
+                # we have found the hash of the buildinfo_filename in the changes file so we can stop
+                # iterating over the changesfile lines
+                break
+    else:
+        print(
+            f"**********ERROR: \tNo binaries found for {package_name} {package_architecture} version {package_version} in {package_series}in any archive pocket."
         )
 
 @click.command()
